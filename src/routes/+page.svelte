98d4693ef685b607db50b24727274803bd584720
ocluf <fulco.taen@gmail.com>
2024-09-11 14:26:43 +0200

    x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,6 @@
 
 <div class="flex flex-col h-screen">
 	<Header />
-	{#if settings.settings.password && settings.settings.user && settings.settings.server && settings.settings.initialCypher}
-		<Editor />
-	{:else}
-		<div class="flex flex-col items-center justify-center h-screen">
-			<h1 class="text-4xl font-bold">No proper settings found</h1>
-			<p class="text-lg">Please configure the Neo4j connection settings with the settings button</p>
-		</div>
-	{/if}
+	<Editor />
 	<Footer />
 </div>
